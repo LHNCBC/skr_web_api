90af5b2e7cf22edefc49d886072b47058b411d67
Willie Rogers <wjrogers@mail.nih.gov>
2011-05-16 09:14:06 -0400

    added usage message to test wrapper script.

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 [jvmoptions] class [programoptions]"
+    exit 0
+fi
+
 
 CP=lib/httpclient-4.1.1.jar:lib/httpclient-cache-4.1.1.jar
 CP=$CP:lib/httpcore-4.1.jar:lib/httpcore-nio-4.1.jar:lib/httpmime-4.1.1.jar
